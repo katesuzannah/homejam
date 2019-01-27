33c6cc6981c08c614cb9b10e8b7fc0de1acb794c
mzguimaraes <mzg218@nyu.edu>
2019-01-27 13:14:11 -0500

    custom events

---
+++ HouseOfMouse/Assets/Scripts/Events/MessengerEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MessageParameters {
+	readonly float enterValue;
+	readonly float enterLerpTime;
+	readonly float exitValue;
+	readonly float exitLerpTime;
+
+	public MessageParameters(float enterVal, float enterTime, float exitVal, float exitTime) {
+		enterValue = enterVal;
+		enterLerpTime = enterTime;
+		exitValue = exitVal;
+		exitLerpTime = exitTime;
+	}
+}
+
+public class MessengerEvent : EnterExitControl {
+
+	public GameObject messageReceiver;
+
+	MessageParameters parameters;
+
+	protected override void Start() {
+		base.Start();
+
+		parameters = new MessageParameters(enterValue, enterLerpTime, exitValue, exitLerpTime);
+	}
+
+	protected override void FireEnter() {
+		messageReceiver.SendMessage("OnEventTriggerVolumeEnter", parameters);
+	}
+
+	protected override void FireExit() {
+		messageReceiver.SendMessage("OnEventTriggerVolumeExit", parameters);
+	}
+}
+
+public interface MessengerEventReceiver {
+	void OnEventTriggerVolumeEnter(MessageParameters parameters);
+	void OnEventTriggerVolumeExit(MessageParameters parameters);
+}
