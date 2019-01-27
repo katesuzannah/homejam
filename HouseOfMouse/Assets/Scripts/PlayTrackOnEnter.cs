54a28a16112a420fca8e1c9a255d9c4245f31ceb
mzguimaraes <mzg218@nyu.edu>
2019-01-27 16:16:26 -0500

    bedroom trigger

---
+++ HouseOfMouse/Assets/Scripts/PlayTrackOnEnter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayTrackOnEnter : MonoBehaviour
+{
+
+	AudioSource source;
+
+	bool played;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		source = GetComponent<AudioSource>();
+    }
+
+	private void OnTriggerEnter(Collider other) {
+		if (!played && other.GetComponent<PlayerTriggerController>() != null) {
+			played = true;
+			source.Play();
+		}
+	}
+}
