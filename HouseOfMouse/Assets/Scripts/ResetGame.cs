2736e3187bfce4bc92c8fdbed10195ceb936b19e
mzguimaraes <mzg218@nyu.edu>
2019-01-27 17:07:01 -0500

    resetter

---
+++ HouseOfMouse/Assets/Scripts/ResetGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+{
+
+	public float timeout = 3f;
+	int escCount = 0;
+	float timeSinceLastPress = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+			escCount++;
+		}
+
+		if (escCount >= 3) {
+			escCount = 0;
+			SceneManager.LoadScene(0);
+		}
+
+		if (escCount > 0f) {
+			timeSinceLastPress += Time.deltaTime;
+			if (timeSinceLastPress >= timeout) {
+				timeSinceLastPress = 0f;
+			}
+		}
+	}
+}
