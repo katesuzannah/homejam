9e31a9bbb23870de0593454f11bd2b2994223778
koliac <ly657@nyu.edu>
2019-01-27 13:00:33 -0500

    Color Assigner Imported

---
+++ HouseOfMouse/Assets/Longkun/Scripts/DesaturationEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+[ImageEffectAllowedInSceneView]
+public class DesaturationEffect : MonoBehaviour
+{
+
+  public Material m_renderMat;
+
+  // Start is called before the first frame update
+  void Start() {
+    if (!SystemInfo.supportsImageEffects || null == m_renderMat ||
+           null == m_renderMat.shader || !m_renderMat.shader.isSupported) {
+      enabled = false;
+      return;
+    }
+  }
+    // Update is called once per frame
+    void Update() {
+
+    }
+  private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+    if (m_renderMat != null) {
+      Graphics.Blit(source, destination, m_renderMat);
+    }
+  }
+
+
+
+}
---
+++ HouseOfMouse/Assets/Longkun/Scripts/MaterialPropertyUtil.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialPropertyUtil 
+{
+  public static IEnumerator LerpMaterialFloat(Material mat, int floatPropertyID, float from, float to, float duration) {
+    float t = 0;
+    while (t < duration) {
+      float f = Mathf.Lerp(from, to, t / duration);
+      SetMaterialFloat(mat, floatPropertyID, f);
+
+
+
+      t += Time.deltaTime;
+      yield return null;
+    }
+
+  }
+
+  public static void SetMaterialFloat(Material mat, int floatPropertyID, float floatVal) {
+    if (mat.HasProperty(floatPropertyID)) {
+      mat.SetFloat(floatPropertyID, floatVal);
+    }
+  }
+
+  public static void SetPropertyBlockFloat(Renderer renderer, int floatPropertyID, float floatVal) {
+    MaterialPropertyBlock block = new MaterialPropertyBlock();
+    block.SetFloat(floatPropertyID, floatVal);
+    renderer.SetPropertyBlock(block);
+
+    
+  }
+  public static IEnumerator LerpPropertyBlockFloat(Renderer renderer, int floatPropertyID, float from, float to, float duration) {
+    float t = 0;
+    while (t < duration) {
+      float f = Mathf.Lerp(from, to, t / duration);
+      SetPropertyBlockFloat(renderer, floatPropertyID, f);
+
+
+
+      t += Time.deltaTime;
+      yield return null;
+    }
+
+  }
+}
