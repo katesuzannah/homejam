26b499ff4adc196fe1e596fb9e3be899a1728e3c
mzguimaraes <mzg218@nyu.edu>
2019-01-27 15:58:04 -0500

    some audio and new skybox

---
+++ HouseOfMouse/Assets/Scripts/ClickSFXPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ClickSFXPlayer : MonoBehaviour
+{
+
+	public List<AudioClip> clips;
+	int lastPlayedIndex;
+	AudioSource source;
+
+	private void Start() {
+		source = GetComponent<AudioSource>();
+	}
+
+	public void PlaySFX() {
+		int newIndex = Random.Range(0, clips.Count - 1);
+
+		while (newIndex == lastPlayedIndex) {
+			newIndex = Random.Range(0, clips.Count - 1);
+		}
+
+		source.PlayOneShot(clips[newIndex]);
+		lastPlayedIndex = newIndex;
+	}
+}
