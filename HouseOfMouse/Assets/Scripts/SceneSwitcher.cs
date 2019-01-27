b4e77542582494d57a80bb4c9965c217bf1a5849
Kate Smith <katesuzannah@gmail.com>
2019-01-26 21:36:15 -0500

    Added start menu

---
+++ HouseOfMouse/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+    public void EndGame() {
+        Application.Quit();
+    }
+}
